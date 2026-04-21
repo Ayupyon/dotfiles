e3795e4fc6e5b82119ef5ae59ac8d85255217664
rin <ayupyon@126.com>
2026-04-21 11:32:38 +0800

    添加wezterm配置

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Latte'
+config.font = wezterm.font("Maple Mono NF CN")
+
+return config
