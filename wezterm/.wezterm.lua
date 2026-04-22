216f6dd3f42773e5937b06506722b5d6117710f8
rin <ayupyon@126.com>
2026-04-22 14:53:41 +0800

    add adjust_window_size_when_changing_font_size and format file

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = "Catppuccin Latte"
 config.font = wezterm.font("Maple Mono NF CN")
+config.adjust_window_size_when_changing_font_size = false
 
 return config
