4b6995c848f3c8e30448c3a5cd91b90865ec202c
rin <ayupyon@126.com>
2026-04-26 15:55:36 +0800

    add wezterm default shell config in windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,4 +5,9 @@ config.color_scheme = "Catppuccin Latte"
 config.font = wezterm.font("Maple Mono NF CN")
 config.adjust_window_size_when_changing_font_size = false
 
+local is_windows = wezterm.target_triple:find("windows") ~= nil
+if is_windows then
+  config.default_prog = { "pwsh.exe" }
+end
+
 return config
