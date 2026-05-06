c31ac7a4dc3bc37d8e4c29dab22a163ec7ce1f07
rin <ayupyon@126.com>
2026-05-06 18:30:28 +0800

    change wezterm theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("Maple Mono NF CN")
 config.adjust_window_size_when_changing_font_size = false
 
