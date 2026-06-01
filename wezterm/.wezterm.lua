878e88613c03aebff86422c6644b5fba074a6590
rin <ayupyon@126.com>
2026-06-01 15:05:50 +0800

    feat: change font to JetBrains Maple Mono NF NL

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("Maple Mono NF CN")
+config.font = wezterm.font("JetBrains Maple Mono")
 config.adjust_window_size_when_changing_font_size = false
 
 local is_windows = wezterm.target_triple:find("windows") ~= nil
